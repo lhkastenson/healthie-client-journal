f6bef1c6cc1880742c78b27ebc1aadd2c79071c9
Lon Kastenson <lon@Lons-MacBook-Air.local>
2026-05-28 16:48:36 -0400

    Add CreateProviders migration

---
+++ db/migrate/20260528204125_create_providers.rb
@@ -0,0 +1,11 @@
+class CreateProviders < ActiveRecord::Migration[8.1]
+  def change
+    create_table :providers do |t|
+      t.string :name
+      t.string :email
+
+      t.timestamps
+    end
+    add_index :providers, :email, unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 0) do
+ActiveRecord::Schema[8.1].define(version: 2026_05_28_204125) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
+  create_table "providers", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "email"
+    t.string "name"
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_providers_on_email", unique: true
+  end
 end
