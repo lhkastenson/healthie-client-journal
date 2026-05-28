a79f076ebc3af911342805a5a066af30b2731ec3
Lon Kastenson <lhkastenson@gmail.com>
2026-05-28 16:59:02 -0400

    Add CreateClients migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_05_28_204125) do
+ActiveRecord::Schema[8.1].define(version: 2026_05_28_205723) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "clients", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "email"
+    t.string "name"
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_clients_on_email", unique: true
+  end
 
   create_table "providers", force: :cascade do |t|
     t.datetime "created_at", null: false
