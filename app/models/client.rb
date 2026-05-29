ff3597418b275101b2420e53458e8e49bbf89074
Lon Kastenson <lhkastenson@gmail.com>
2026-05-29 01:53:07 -0400

    Add client relationships and validations

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,2 +1,8 @@
 class Client < ApplicationRecord
+  has_many :enrollments, dependent: :destroy
+  has_many :providers, through: :enrollments
+  has_many :journal_entries, dependent: :destroy
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
 end
