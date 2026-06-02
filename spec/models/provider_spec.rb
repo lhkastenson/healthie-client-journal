f9b6485202d80e3ac7b982794aeada4a20b80eab
Lon Kastenson <lhkastenson@gmail.com>
2026-06-01 20:13:32 -0400

    Add provider spec

--- spec/models/provider_spec.rb
+++ spec/models/provider_spec.rb
@@ -11,6 +11,17 @@ RSpec.describe Provider, type: :model do
       provider = build(:provider, name: nil)
       expect(provider).not_to be_valid
     end
+
+    it "is invalid without an email" do
+      provider = build(:provider, email: nil)
+      expect(provider).not_to be_valid
+    end
+
+    it "is invalid with a duplicate email" do
+      create(:provider, email: "test@test.com")
+      provider = build(:provider, email: "test@test.com")
+      expect(provider).not_to be_valid
+    end
   end
 
   describe ".clients_for" do
@@ -20,6 +31,17 @@ RSpec.describe Provider, type: :model do
       create(:enrollment, provider: provider, client: client)
 
       expect(Provider.clients_for(provider.id)).to include(client)
+    end
+  end
+
+  describe ".journal_entries_for" do
+    it "returns all journal_entries for a given provider" do
+      provider = create(:provider)
+      client = create(:client)
+      create(:enrollment, provider: provider, client: client)
+      journal_entry = create(:journal_entry, client: client)
+
+      expect(Provider.journal_entries_for(provider.id)).to include(journal_entry)
     end
   end
 end
